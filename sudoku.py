cf2385dacdafde07674bd24f8d683b96de0be1c7
Script Raccoon <scriptraccoon@gmail.com>
2023-11-25 20:24:13 +0100

    complete refactor, improve perf

--- sudoku.py
+++ sudoku.py
@@ -6,14 +6,17 @@ from time import perf_counter
 
 
 def key(row: int, col: int) -> str:
-    """Encode coordinates such as (3,1) with the string 31"""
-    return f"{row}{col}"
+    """Encodes a coordinate such as (3,1) with the string 31"""
+    return str(row) + str(col)
 
 
 class Sudoku:
     """Sudoku class"""
 
-    peer_dict: dict[str, set[str]] = {
+    coords = [key(row, col) for row in range(9) for col in range(9)]
+    """List of all coordinates, encoded as strings"""
+
+    peers: dict[str, set[str]] = {
         key(row, col): set.union(
             {key(i, col) for i in range(9) if i != row},
             {key(row, j) for j in range(9) if j != col},
@@ -31,28 +34,27 @@ class Sudoku:
 
     def __init__(
         self,
-        value_dict: dict[str, int],
-        candidate_dict: dict[str, str] | None = None,
+        values: dict[str, int],
+        candidates: dict[str, str] | None = None,
     ) -> None:
         """Initialize Sudoku with a value and a candidate dictionary"""
-        self.value_dict = value_dict
-        self.coords = value_dict.keys()
+        self.values = values
         self.has_contradiction = False
 
-        if candidate_dict is None:
-            self.candidate_dict = self.get_candidate_board()
+        if candidates is None:
+            self.candidates = self.get_candidate_board()
         else:
-            self.candidate_dict = candidate_dict
+            self.candidates = candidates
 
     @staticmethod
     def generate_from_board(
         board: list[list[int]],
     ) -> Sudoku:
         """Generates a Sudoku object from a given 2-dimensional list of integers"""
-        value_dict = {
+        values = {
             key(row, col): board[row][col] for row in range(9) for col in range(9)
         }
-        return Sudoku(value_dict, None)
+        return Sudoku(values)
 
     @staticmethod
     def generate_from_string(line: str) -> Sudoku:
@@ -60,86 +62,78 @@ class Sudoku:
         line = line.replace("\n", "")
         assert len(line) == 81
 
-        def to_num(c: str) -> int:
+        def to_digit(c: str) -> int:
             return int(c) if c.isnumeric() else 0
 
-        value_dict = {
-            key(row, col): to_num(line[row * 9 + col])
+        values = {
+            key(row, col): to_digit(line[row * 9 + col])
             for row in range(9)
             for col in range(9)
         }
-        return Sudoku(value_dict)
+        return Sudoku(values)
 
     def copy(self) -> Sudoku:
         """Generates a copy of the given Sudoku"""
-        return Sudoku(self.value_dict.copy(), self.candidate_dict.copy())
+        return Sudoku(self.values.copy(), self.candidates.copy())
 
-    def print(self) -> None:
+    def __str__(self) -> str:
         """Prints the Sudoku in a nice way to the console"""
-        print(" " + "-" * 23)
+        output = " " + "-" * 23 + "\n"
         for row in range(9):
             for col in range(9):
-                num = self.value_dict[key(row, col)]
+                digit = self.values[key(row, col)]
                 if col == 0:
-                    print("| ", end="")
-                print(num if num > 0 else ".", end="")
-                print(" ", end="")
+                    output += "| "
+                output += (str(digit) if digit > 0 else ".") + " "
                 if col % 3 == 2:
-                    print("| ", end="")
-            print()
+                    output += "| "
+            output += "\n"
             if row % 3 == 2:
-                print(" " + "-" * 23)
-        print()
+                output += " " + "-" * 23 + "\n"
+        output += "\n"
+        return output
 
-    def candidates(self, _key: str) -> str:
+    def get_candidates(self, coord: str) -> str:
         """Generates the list (encoded as a string) of candidates at a position"""
-        num = self.value_dict[_key]
-        if num != 0:
-            return str(num)
-        values_of_peers = {self.value_dict[peer] for peer in Sudoku.peer_dict[_key]}
-        result = ""
-        for n in range(1, 10):
-            if n not in values_of_peers:
-                result += str(n)
-        return result
+        digit = self.values[coord]
+        if digit != 0:
+            return str(digit)
+        values_of_peers = {self.values[peer] for peer in Sudoku.peers[coord]}
+        return "".join([str(n) for n in range(1, 10) if n not in values_of_peers])
 
     def get_candidate_board(self) -> dict[str, str]:
         """Returns the dictionary of candidates over all coordinates"""
-        return {coord: self.candidates(coord) for coord in self.coords}
+        return {coord: self.get_candidates(coord) for coord in Sudoku.coords}
 
     def get_next_coord(self) -> str | None:
-        """Returns the open coordinate with the least number of candidates"""
-
-        candidate_list = [
-            (coord, len(self.candidate_dict[coord]))
-            for coord in self.coords
-            if self.value_dict[coord] == 0
-        ]
-
-        if not candidate_list:
+        """Returns the free coordinate with the least number of candidates"""
+        try:
+            return min(
+                [coord for coord in Sudoku.coords if self.values[coord] == 0],
+                key=lambda coord: len(self.candidates[coord]),
+            )
+        except:
             return None
-        return min(candidate_list, key=lambda x: x[1])[0]
 
-    def remove_candidate(self, coord: str, num: int) -> None:
+    def remove_candidate(self, coord: str, digit: int) -> None:
         """Removes a candidate from a coordinate (in case it's there),
         detects if a contradiction happens, and if a single candidate
         is left this one is set."""
-        if str(num) not in self.candidate_dict[coord]:
+        if str(digit) not in self.candidates[coord]:
             return
-        self.candidate_dict[coord] = self.candidate_dict[coord].replace(str(num), "")
-        if len(self.candidate_dict[coord]) == 0:
+        self.candidates[coord] = self.candidates[coord].replace(str(digit), "")
+        if not self.candidates[coord]:
             self.has_contradiction = True
-        elif len(self.candidate_dict[coord]) == 1:
-            unique_candidate = int(self.candidate_dict[coord])
-            self.set_number(coord, unique_candidate)
+        elif len(self.candidates[coord]) == 1:
+            self.set_digit(coord, int(self.candidates[coord]))
 
-    def set_number(self, coord: str, num: int) -> None:
-        """Sets a number at a given coordinate (via its key), and removes
-        that number from the candidates of the coordinate's peers"""
-        self.value_dict[coord] = num
-        self.candidate_dict[coord] = str(num)
-        for peer in Sudoku.peer_dict[coord]:
-            self.remove_candidate(peer, num)
+    def set_digit(self, coord: str, digit: int) -> None:
+        """Sets a digit at a given coordinate and removes that digit
+        from the candidates of the coordinate's peers"""
+        self.values[coord] = digit
+        self.candidates[coord] = str(digit)
+        for peer in Sudoku.peers[coord]:
+            self.remove_candidate(peer, digit)
             if self.has_contradiction:
                 return
 
@@ -149,40 +143,48 @@ class Sudoku:
         if not coord:
             yield self
             return
-
-        for num in self.candidate_dict[coord]:
+        for num in self.candidates[coord]:
             copy = self.copy()
-            copy.set_number(coord, int(num))
+            copy.set_digit(coord, int(num))
             if not copy.has_contradiction:
                 yield from copy.solutions()
 
 
-def main():
-    """Prints the solutions of hard sample Sudokus and measures the speed"""
+def measure_time():
+    """Solves all sudoku samples and measures the time"""
     sudoku_counter = 0
+    total = 0
     with open("performance.txt", "w", encoding="utf8") as output:
         with open("samples.txt", "r", encoding="utf8") as file:
             for line in file:
                 if line.startswith("#"):
                     continue
                 sudoku_counter += 1
-                print(f"Sudoku #{sudoku_counter}")
                 sudoku = Sudoku.generate_from_string(line)
-                sudoku.print()
-
-                sol_counter = 0
+                print("solving sudoku", sudoku_counter)
                 start = perf_counter()
-                sols = sudoku.solutions()
-
-                for sol in sols:
-                    sol.print()
-                    sol_counter += 1
-
+                list(sudoku.solutions())
                 end = perf_counter()
-                print(f"Found {sol_counter} solutions")
-                print("Elapsed time: ", end - start, "\n")
                 output.write(str(end - start) + "\n")
+                total += end - start
+        output.write("total: " + str(total))
+    print("results written to performance.txt")
+
+
+def solve_sample():
+    """Prints the solutions of a sample Sudoku"""
+    sample = "48.3............71.2.......7.5....6....2..8.............1.76...3.....4......5...."
+    sudoku = Sudoku.generate_from_string(sample)
+    print("Sample:")
+    print(sudoku)
+    print("Solutions:")
+    start = perf_counter()
+    for sol in sudoku.solutions():
+        print(sol)
+    end = perf_counter()
+    print("Elapsed time: ", end - start, "\n")
 
 
 if __name__ == "__main__":
-    main()
+    solve_sample()
+    # measure_time()
